c189afe0997cecf3b41e37ffc6e551be5ca6a766
Inacio <inacioloy@gmail.com>
2017-11-09 23:12:28 -0300

    adiciona projeto

---
+++ academico/src/academico/Academico.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academico;
+
+/**
+ *
+ * @author inacio
+ */
+public class Academico {
+
+    /**
+     * @param args the command line arguments
+     */
+    //******************************************************
+
+//Instituto Federal de São Paulo - Campus Sertãozinho
+
+//Disciplina......: M3LPBD
+
+//Programação de Computadores e Dispositivos Móveis
+
+//Aluno...........: Inácio de Loiola Souza Silva
+
+//******************************************************
+    
+    public static void main(String[] args) {
+    Principal programa = new Principal();
+    programa.setVisible(true);
+    }
+    
+}
